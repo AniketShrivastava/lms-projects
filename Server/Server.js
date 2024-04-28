25b63f8bc618d2a4dc720689028ea2648c55e1e9
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-04-29 02:05:31 +0530

    update server file

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,11 @@
 import app from "./app.js";
+import connectToDB from "./config/dbConfig.js";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5011;
 
 app.listen(PORT, async () => {
     // Connect to DB
-    // await connectToDB();
+    await connectToDB();
     console.log(`App is running at http://localhost:${PORT}`);
   });
