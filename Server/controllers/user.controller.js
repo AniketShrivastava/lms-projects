9a78cd5e90a4cf2931525bc7f139581f5a0d3830
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-04-28 13:54:39 +0530

    conrollers setup create

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,16 @@
+const register = (req,res)=>{
+
+}
+const login = (req,res)=>{
+    
+}
+const logout = (req,res)=>{
+    
+}
+const getProfile = (req,res)=>{
+    
+}
+
+module.exports={
+    register,login,logout,getProfile
+}
